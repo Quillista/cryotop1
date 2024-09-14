68409a96e90f71e5bb9114838912e6f311600fc8
Quillista <113875212+Quillista@users.noreply.github.com>
2024-09-13 19:09:06 -0500

    Add files via upload

---
+++ escenario 1/Cliente/Cliente/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
+
+class Client
+{
+    static void Main()
+    {
+        try
+        {
+          
+            // Se inicializa el cliente
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[1];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sender.Connect(remoteEP);
+
+            //se solicita la llave al servidor para cifrar el mensaje
+            Console.WriteLine("Conectado al servidor. Recibiendo llave...");
+            sender.Send(Encoding.ASCII.GetBytes("dame la llave"));
+            // Recibir llave
+            byte[] buffer2 = new byte[1024];
+            int bytesRec2 = sender.Receive(buffer2);
+            var llaveult = Encoding.ASCII.GetString(buffer2, 0, bytesRec2);
+            // Clave secreta de 32 bytes para Salsa20
+            byte[] key = Encoding.ASCII.GetBytes(llaveult); // 32 bytes key
+            byte[] nonce = Encoding.ASCII.GetBytes("12345678"); // 8 bytes nonce
+
+            // Mensaje original
+            Console.WriteLine("Por favor, ingrese su mensaje:");
+
+            while (true)
+            {
+                // Leer lo que el usuario escribe y guardarlo en una variable
+                // Mensaje original
+                Console.WriteLine("Por favor, ingrese su mensaje:");
+                string mensajerec = Console.ReadLine();
+                string message = mensajerec;
+                byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+
+                // Cifrar con Salsa20
+                byte[] encryptedMessage = EncryptSalsa20(messageBytes, key, nonce);
+
+                Console.WriteLine("Conectado al servidor. Enviando mensaje cifrado...");
+
+                // Enviar el mensaje cifrado
+                sender.Send(encryptedMessage);
+
+                // Recibir respuesta del servidor
+                byte[] buffer = new byte[1024];
+                int bytesRec = sender.Receive(buffer);
+                Console.WriteLine("Respuesta del servidor: {0}", Encoding.ASCII.GetString(buffer, 0, bytesRec));
+
+            }
+          
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    //Funcion para cifrar usando Salsa20,usamos la biblioteca BouncyCastle en c#
+    public static byte[] EncryptSalsa20(byte[] plainText, byte[] key, byte[] nonce)
+    {
+        Salsa20Engine engine = new Salsa20Engine(); // Salsa20
+        ParametersWithIV parameters = new ParametersWithIV(new KeyParameter(key), nonce);
+
+        engine.Init(true, parameters); // true para cifrar
+        byte[] cipherText = new byte[plainText.Length];
+        engine.ProcessBytes(plainText, 0, plainText.Length, cipherText, 0);
+
+        return cipherText;
+    }
+}
---
+++ escenario 1/socket/socket/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
+
+class Server
+{
+    static void Main()
+    {
+        //se inicializa el servidor
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+        Socket listener = new Socket(IPAddress.Any.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            Console.WriteLine("Esperando conexión...");
+
+            Socket handler = listener.Accept();
+
+            // Recibe mensaje cifrado
+            byte[] buffer2 = new byte[1024];
+            int bytesReceived2 = handler.Receive(buffer2);
+            //el cliente solicita la llave y el servidor procede a enviarla por sockets
+
+            Console.WriteLine("Enviando llave...");
+            // Clave secreta de 32 bytes y nonce de 8 bytes (que sera enviada al cliente)
+            byte[] key = Encoding.ASCII.GetBytes("12345678901234567890123456789012");
+            byte[] nonce = Encoding.ASCII.GetBytes("12345678");
+            handler.Send(key);
+            while (true)
+            {
+                // Recibe mensaje cifrado
+                byte[] buffer = new byte[1024];
+                int bytesReceived = handler.Receive(buffer);
+                // Descifrar el mensaje
+                byte[] encryptedMessage = new byte[bytesReceived];
+                Array.Copy(buffer, encryptedMessage, bytesReceived);
+                //se usa salsa20 para descifrarlo
+                byte[] decryptedMessage = DecryptSalsa20(encryptedMessage, key, nonce);
+
+                Console.WriteLine("Mensaje recibido y descifrado: {0}", Encoding.ASCII.GetString(decryptedMessage));
+
+                // Responder al cliente
+                byte[] msg = Encoding.ASCII.GetBytes("Mensaje recibido en el servidor.");
+                handler.Send(msg);
+
+            }
+            //handler.Shutdown(SocketShutdown.Both);
+            //handler.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("Servidor cerrado.");
+    }
+
+
+    // Función para descifrar usando Salsa20, usamos la biblioteca BouncyCastle en c#
+
+    public static byte[] DecryptSalsa20(byte[] cipherText, byte[] key, byte[] nonce)
+    {
+        Salsa20Engine engine = new Salsa20Engine(); // Salsa20
+        ParametersWithIV parameters = new ParametersWithIV(new KeyParameter(key), nonce);
+
+        engine.Init(false, parameters); // false para descifrar
+        byte[] plainText = new byte[cipherText.Length];
+        engine.ProcessBytes(cipherText, 0, cipherText.Length, plainText, 0);
+
+        return plainText;
+    }
+}
